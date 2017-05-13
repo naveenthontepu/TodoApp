cda720edd105bcce67fef1212a4b78efc650d4cc
naveen thontepu <tnaveen.leo@gmail.com>
2017-05-13 13:55:35 +0530

    add todo from post man

---
+++ server/server.js
@@ -0,0 +1,31 @@
+/**
+ * Created by mac on 5/13/17.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=> {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=> {
+        console.log(JSON.stringify(doc, undefined, 2));
+        res.send(doc);
+    }, (e)=> {
+        console.log('Unable to save', e);
+        res.status(400).send(e);
+    })
+});
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000');
+});
